ad89e5d60393fcf49347d8e363372046e7e9d6f6
Ratu Atikah N <ratuuatikahn30@gmail.com>
2022-04-08 11:44:46 +0700

    Praktikum DB SQLite

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:sqlite_flutter/pages/item_list.dart';
+import 'package:sqlite_flutter/pages/entry_form.dart';
+
+import '../helpers/db_helper.dart';
+import '../models/item.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var db = DBHelper();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (BuildContext context) => EntryForm(
+                  Item('', '', 0, 0),
+                  true,
+                ),
+              ),
+            );
+          },
+          child: const Icon(Icons.add),
+        ),
+        appBar: AppBar(
+          title:
+              const Text('Daftar Item\nRatu Atikah Nurissobach - 2031710145'),
+        ),
+        body: FutureBuilder(
+          future: db.getItem(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              print(snapshot.error);
+            }
+
+            var data = snapshot.data;
+
+            return snapshot.hasData
+                ? ItemList(data as List<Item>)
+                : const Center(
+                    child: Text('Tidak Ada Data\nRatu Atikah Nurissobach'),
+                  );
+          },
+        ));
+  }
+}
